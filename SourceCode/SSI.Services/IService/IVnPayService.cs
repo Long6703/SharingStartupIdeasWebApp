f11833ee648d6ad42a087f2cd6e5ce017a90035b
CoderMakeDream <ductungpro121407@gmail.com>
2024-10-10 20:39:14 +0700

    vnpay payment

---
+++ SourceCode/SSI.Services/IService/IVnPayService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using SSI.Share.ViewModel;
+
+namespace SSI.Services.IService
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(HttpContext context, VnPaymentRequestModel model);
+        VnPaymentResponseModel PaymentExecute(IQueryCollection collections);
+    }
+}
